b00c33e5dfa89d160901623977284a488be3dca4
Leah Kiner <leki4360@colorado.edu>
2022-10-26 11:47:00 -0600

    First commit to prescribed motion module. Code compiles but unit test is not working yet
    Former-commit-id: 92917eac0c71a83d893fa96f271caf6a11d36c61
    Former-commit-id: 76dbacd97a4904fd09dbc5ed4bc1d35a0d7778e9

---
+++ src/simulation/dynamics/prescribedMotion/_UnitTest/test_prescribedMotionStateEffector.py
@@ -0,0 +1,323 @@
+# ISC License
+#
+# Copyright (c) 2022, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+
+#
+#   Unit Test Script
+#   Module Name:        prescribedMotion
+#   Author:             Leah Kiner
+#   Creation Date:      October 11, 2022
+#
+
+import inspect
+import os
+
+import numpy as np
+
+filename = inspect.getframeinfo(inspect.currentframe()).filename
+path = os.path.dirname(os.path.abspath(filename))
+splitPath = path.split('simulation')
+
+from Basilisk.utilities import SimulationBaseClass
+from Basilisk.utilities import unitTestSupport  # general support file with common unit test functions
+import matplotlib.pyplot as plt
+from Basilisk.simulation import spacecraft, prescribedMotionStateEffector, gravityEffector
+from Basilisk.utilities import macros
+
+
+# uncomment this line is this test is to be skipped in the global unit test run, adjust message as needed
+# @pytest.mark.skipif(conditionstring)
+# uncomment this line if this test has an expected failure, adjust message as needed
+# @pytest.mark.xfail() # need to update how the RW states are defined
+# provide a unique test method name, starting with test_
+
+
+def prescribedMotionTest(show_plots):
+    r"""
+    **Validation Test Description**
+
+    This unit test sets up a spacecraft with a single-axis rotating rigid body attached to a rigid hub. The spinning
+    body's center of mass is off-center from the spinning axis and the position of the axis is arbitrary. The scenario
+    includes gravity acting on both the spacecraft and the effector.
+
+    **Description of Variables Being Tested**
+
+    In this file we are checking the principles of conservation of energy and angular momentum. Both the orbital and
+    rotational energy and angular momentum must be maintained when conservative forces like gravity are present.
+    Therefore, the values of the variables
+
+    - ``finalOrbAngMom``
+    - ``finalOrbEnergy``
+    - ``finalRotAngMom``
+
+    against their initial values.
+    """
+    [testResults, testMessage] = test_prescribedMotion(show_plots)
+    assert testResults < 1, testMessage
+
+
+def test_prescribedMotion(show_plots):
+    __tracebackhide__ = True
+
+    testFailCount = 0  # zero unit test result counter
+    testMessages = []  # create empty list to store test log messages
+
+    scObject = spacecraft.Spacecraft()
+    scObject.ModelTag = "spacecraftBody"
+
+    # Define mass properties of the rigid hub of the spacecraft
+    scObject.hub.mHub = 750.0
+    scObject.hub.r_BcB_B = [[0.0], [0.0], [0.5]]
+    scObject.hub.IHubPntBc_B = [[900.0, 0.0, 0.0], [0.0, 800.0, 0.0], [0.0, 0.0, 600.0]]
+
+    # Set the initial values for the states
+    scObject.hub.r_CN_NInit = [[-4020338.690396649], [7490566.741852513], [5248299.211589362]]
+    scObject.hub.v_CN_NInit = [[-5199.77710904224], [-3436.681645356935], [1041.576797498721]]
+    scObject.hub.sigma_BNInit = [[0.0], [0.0], [0.0]]
+    scObject.hub.omega_BN_BInit = [[0.0], [0.0], [0.0]]
+
+    unitTaskName = "unitTask"  # arbitrary name (don't change)
+    unitProcessName = "TestProcess"  # arbitrary name (don't change)
+
+    # Create a sim module as an empty container
+    unitTestSim = SimulationBaseClass.SimBaseClass()
+
+    # Create test thread
+    testIncrement = 0.001
+    testProcessRate = macros.sec2nano(testIncrement)  # update process rate update time
+    testProc = unitTestSim.CreateNewProcess(unitProcessName)
+    testProc.addTask(unitTestSim.CreateNewTask(unitTaskName, testProcessRate))
+
+    # Create prescribed state effector
+    simTime1 = 30.0
+    simTime2 = 60.0 + testIncrement
+
+    platform = prescribedMotionStateEffector.PrescribedMotionStateEffector()
+
+    # Choose principal body axis for prescribed rotation
+    rotAxisNum = 0  # rotation about first body axis
+    # rotAxisNum = 1 # rotation about second body axis
+    # rotAxisNum = 2 # rotation about third body axis
+
+    # Define properties of state effector
+    platform.mass = 100.0
+    platform.IHubBc_B = scObject.hub.IHubPntBc_B
+    platform.IPntFc_F = [[50.0, 0.0, 0.0], [0.0, 50.0, 0.0], [0.0, 0.0, 50.0]]
+    platform.dcm_F0B = [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]
+    platform.r_MB_B = [[0.0], [0.0], [1.0]]
+    platform.r_FcF_F = [[0.0], [0.0], [1.0]]
+    platform.theta_FBInit = 0.0
+    platform.thetaDot_FBInit = 0.0
+    platform.ModelTag = "Platform"
+    platform.rotAxisNum = rotAxisNum
+
+    # Add platform to spacecraft
+    scObject.addStateEffector(platform)
+
+    # Add test module to runtime call list
+    unitTestSim.AddModelToTask(unitTaskName, scObject)
+    unitTestSim.AddModelToTask(unitTaskName, platform)
+
+    # Add Earth gravity to the simulation
+    earthGravBody = gravityEffector.GravBodyData()
+    earthGravBody.planetName = "earth_planet_data"
+    earthGravBody.mu = 0.3986004415E+15  # meters!
+    earthGravBody.isCentralBody = True
+    earthGravBody.useSphericalHarmParams = False
+    scObject.gravField.gravBodies = spacecraft.GravBodyVector([earthGravBody])
+
+    # Log the spacecraft state message
+    scStateData = scObject.scStateOutMsg.recorder()
+    unitTestSim.AddModelToTask(unitTaskName, scStateData)
+
+    # Initialize the simulation
+    unitTestSim.InitializeSimulation()
+
+    # Add energy and momentum variables to log
+    unitTestSim.AddVariableForLogging(scObject.ModelTag + ".totOrbEnergy", testProcessRate, 0, 0, 'double')
+    unitTestSim.AddVariableForLogging(scObject.ModelTag + ".totOrbAngMomPntN_N", testProcessRate, 0, 2, 'double')
+    unitTestSim.AddVariableForLogging(scObject.ModelTag + ".totRotAngMomPntC_N", testProcessRate, 0, 2, 'double')
+
+    # Add states to log
+    prescribedStateData = platform.prescribedMotionOutMsg.recorder()
+    unitTestSim.AddModelToTask(unitTaskName, prescribedStateData)
+
+    # Setup and run the simulation
+    platform.u_B = 1.0
+    unitTestSim.ConfigureStopTime(macros.sec2nano(simTime1))
+    unitTestSim.ExecuteSimulation()
+
+    platform.u_B = -1.0
+    unitTestSim.ConfigureStopTime(macros.sec2nano(simTime1 + simTime2))
+    unitTestSim.ExecuteSimulation()
+
+    # Extract the logged variables
+    orbEnergy = unitTestSim.GetLogVariableData(scObject.ModelTag + ".totOrbEnergy")
+    orbAngMom_N = unitTestSim.GetLogVariableData(scObject.ModelTag + ".totOrbAngMomPntN_N")
+    rotAngMom_N = unitTestSim.GetLogVariableData(scObject.ModelTag + ".totRotAngMomPntC_N")
+    omega_FB_F = prescribedStateData.omega_FB_F
+    omega_BN_B = scStateData.omega_BN_B
+
+    # Setup the conservation quantities
+    initialOrbAngMom_N = [[orbAngMom_N[0, 1], orbAngMom_N[0, 2], orbAngMom_N[0, 3]]]
+    finalOrbAngMom = [orbAngMom_N[-1]]
+    initialRotAngMom_N = [[rotAngMom_N[0, 1], rotAngMom_N[0, 2], rotAngMom_N[0, 3]]]
+    finalRotAngMom = [rotAngMom_N[-1]]
+    initialOrbEnergy = [[orbEnergy[0, 1]]]
+    finalOrbEnergy = [orbEnergy[-1]]
+
+    # Calculate analytic results
+    u_B = [] # Empty list to store prescribed control input
+    # theta_FB_Analytic = []
+    # thetaDot_FB_Analytic = []
+    # thetaDDot_FB_Analytic = []
+    theta_BN_Analytic = []
+    thetaDot_BN_Analytic = []
+    # thetaDDot_BN_Analytic = []
+    # omega_FB_B_Analytic = [] # Empty list to store analytic omega_FB_B
+    # omegaDot_FB_B_Analytic = [] # Empty list to store analytic omegaDot_FB_B
+    omega_BN_B_Analytic = [] # Empty list to store analytic omega_BN_B
+    # omegaDot_BN_B_Analytic = []  # Empty list to store analytic omegaDot_BN_B
+    theta_BN_AnalyticInit = 0.0
+
+    for i in range(0, len(prescribedStateData.times())):
+        tt = prescribedStateData.times[:, i]
+        if tt < 0.5 * len(prescribedStateData.times()):
+            u_B[i] = 1.0
+        else:
+            u_B[i] = -1.0
+        # theta_FB_Analytic[i] = (0.5 / (scObject.hub.IHubPntBc_B[rotAxisNum][rotAxisNum] + platform.IPntFc_F[rotAxisNum][rotAxisNum])) * u_B[i] * tt * tt + platform.thetaDot_FBInit * tt + platform.theta_FBInit
+        # thetaDot_FB_Analytic[i] = (1 / (scObject.hub.IHubPntBc_B[rotAxisNum][rotAxisNum] + platform.IPntFc_F[rotAxisNum][rotAxisNum])) * u_B[i] * tt + platform.thetaDot_FBInit
+        # thetaDDot_FB_Analytic[i] = (1 / (scObject.hub.IHubPntBc_B[rotAxisNum][rotAxisNum] + platform.IPntFc_F[rotAxisNum][rotAxisNum])) * u_B[i]
+        # omega_FB_B_Analytic[:, i] = [[0.0], [0.0], [0.0]]
+        # omega_FB_B_Analytic[rotAxisNum, i] = thetaDot_FB_Analytic[i]
+        # omegaDot_FB_B_Analytic[:, i] = [[0.0], [0.0], [0.0]]
+        # omegaDot_FB_B_Analytic[rotAxisNum, i] = thetaDDot_FB_Analytic[i]
+        theta_BN_Analytic[i] = ( -0.5 / scObject.hub.IHubPntBc_B[rotAxisNum][rotAxisNum] ) * u_B[i] * tt * tt + scObject.hub.omega_BN_BInit * tt + theta_BN_AnalyticInit
+        thetaDot_BN_Analytic[i] = ( - 1 / scObject.hub.IHubPntBc_B[rotAxisNum][rotAxisNum] ) * u_B[i] * tt + scObject.hub.omega_BN_BInit
+        # thetaDDot_BN_Analytic[i] = ( - 1 / scObject.hub.IHubPntBc_B[rotAxisNum][rotAxisNum] ) * u_B[i]
+        omega_BN_B_Analytic[:, i] = [[0.0], [0.0], [0.0]]
+        omega_BN_B_Analytic[rotAxisNum, i] = thetaDot_BN_Analytic[i]
+        # omegaDot_BN_B_Analytic[:, i] = [[0.0], [0.0], [0.0]]
+        # omegaDot_BN_B_Analytic[rotAxisNum, i] = thetaDDot_BN_Analytic[i]
+
+    # Plotting: analytic results
+    plt.figure()
+    plt.clf()
+    plt.plot(prescribedStateData.times() * 1e-9, omega_BN_B_Analytic[:, 0],
+             prescribedStateData.times() * 1e-9, omega_BN_B_Analytic[:, 1],
+             prescribedStateData.times() * 1e-9, omega_BN_B_Analytic[:, 2])
+    plt.xlabel('Time (s)')
+    plt.ylabel('Analytic omega_BN_B')
+
+    plt.figure()
+    plt.clf()
+    plt.plot(prescribedStateData.times() * 1e-9, theta_BN_Analytic[:, 0],
+             prescribedStateData.times() * 1e-9, theta_BN_Analytic[:, 1],
+             prescribedStateData.times() * 1e-9, theta_BN_Analytic[:, 2])
+    plt.xlabel('Time (s)')
+    plt.ylabel('Analytic theta_BN_B')
+
+    plt.figure()
+    plt.clf()
+    plt.plot(prescribedStateData.times() * 1e-9, u_B[:, 0],
+             prescribedStateData.times() * 1e-9, u_B[:, 1],
+             prescribedStateData.times() * 1e-9, u_B[:, 2])
+    plt.xlabel('Time (s)')
+    plt.ylabel('Prescribed Control Input Torque')
+
+    # Plotting: Difference between numerical and analytical omega_BN_B
+    plt.figure()
+    plt.clf()
+    plt.plot(prescribedStateData.times() * 1e-9, (omega_BN_B[:, 0] - omega_BN_B_Analytic[:, 0]),
+             prescribedStateData.times() * 1e-9, (omega_BN_B[:, 1] - omega_BN_B_Analytic[:, 1]),
+             prescribedStateData.times() * 1e-9, (omega_BN_B[:, 2] - omega_BN_B_Analytic[:, 2]))
+    plt.xlabel('Time (s)')
+    plt.ylabel('Difference Between Analytic and Simulated omega_BN_B')
+
+    # Plotting: Prescribed effector states
+    plt.figure()
+    plt.clf()
+    plt.plot(prescribedStateData.times() * 1e-9, omega_FB_F[:, 0],
+             prescribedStateData.times() * 1e-9, omega_FB_F[:, 1],
+             prescribedStateData.times() * 1e-9, omega_FB_F[:, 2])
+    plt.xlabel('Time (s)')
+    plt.ylabel('Omega_FB_F Components')
+
+    # Plotting: conservation quantities
+    plt.close("all")
+    plt.figure()
+    plt.clf()
+    plt.plot(orbAngMom_N[:, 0] * 1e-9, (orbAngMom_N[:, 1] - orbAngMom_N[0, 1]) / orbAngMom_N[0, 1],
+             orbAngMom_N[:, 0] * 1e-9, (orbAngMom_N[:, 2] - orbAngMom_N[0, 2]) / orbAngMom_N[0, 2],
+             orbAngMom_N[:, 0] * 1e-9, (orbAngMom_N[:, 3] - orbAngMom_N[0, 3]) / orbAngMom_N[0, 3])
+    plt.xlabel('Time (s)')
+    plt.ylabel('Orbital Angular Momentum Relative Difference')
+
+    plt.figure()
+    plt.clf()
+    plt.plot(orbEnergy[:, 0] * 1e-9, (orbEnergy[:, 1] - orbEnergy[0, 1]) / orbEnergy[0, 1])
+    plt.xlabel('Time (s)')
+    plt.ylabel('Orbital Energy Relative Difference')
+
+    plt.figure()
+    plt.clf()
+    plt.plot(rotAngMom_N[:, 0] * 1e-9, (rotAngMom_N[:, 1] - rotAngMom_N[0, 1]) / rotAngMom_N[0, 1],
+             rotAngMom_N[:, 0] * 1e-9, (rotAngMom_N[:, 2] - rotAngMom_N[0, 2]) / rotAngMom_N[0, 2],
+             rotAngMom_N[:, 0] * 1e-9, (rotAngMom_N[:, 3] - rotAngMom_N[0, 3]) / rotAngMom_N[0, 3])
+    plt.xlabel('Time (s)')
+    plt.ylabel('Rotational Angular Momentum Relative Difference')
+
+    if show_plots:
+        plt.show()
+    plt.close("all")
+
+    # Testing setup
+    accuracy = 1e-12
+    finalOrbAngMom = numpy.delete(finalOrbAngMom, 0, axis=1)  # remove time column
+    finalRotAngMom = numpy.delete(finalRotAngMom, 0, axis=1)  # remove time column
+    finalOrbEnergy = numpy.delete(finalOrbEnergy, 0, axis=1)  # remove time column
+
+    for i in range(0, len(initialOrbAngMom_N)):
+        # check a vector values
+        if not unitTestSupport.isArrayEqualRelative(finalOrbAngMom[i], initialOrbAngMom_N[i], 3, accuracy):
+            testFailCount += 1
+            testMessages.append(
+                "FAILED: Prescribed Motion integrated test failed orbital angular momentum unit test")
+
+    for i in range(0, len(initialRotAngMom_N)):
+        # check a vector values
+        if not unitTestSupport.isArrayEqualRelative(finalRotAngMom[i], initialRotAngMom_N[i], 3, accuracy):
+            testFailCount += 1
+            testMessages.append(
+                "FAILED: Prescribed Motion integrated test failed rotational angular momentum unit test")
+
+    for i in range(0, len(initialOrbEnergy)):
+        # check a vector values
+        if not unitTestSupport.isArrayEqualRelative(finalOrbEnergy[i], initialOrbEnergy[i], 1, accuracy):
+            testFailCount += 1
+            testMessages.append("FAILED: Prescribed Motion integrated test failed orbital energy unit test")
+
+    if testFailCount == 0:
+        print("PASSED: " + " Prescribed Motion gravity integrated test")
+
+    assert testFailCount < 1, testMessages
+    # return fail count and join into a single string all messages in the list
+    # testMessage
+    return [testFailCount, ''.join(testMessages)]
+
+if __name__ == "__main__":
+    prescribedMotionTest(True)
---
+++ src/simulation/dynamics/prescribedMotion/prescribedMotionStateEffector.h
@@ -0,0 +1,136 @@
+/*
+ ISC License
+
+ Copyright (c) 2022, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+ */
+
+#ifndef PRESCRIBED_MOTION_STATE_EFFECTOR_H
+#define PRESCRIBED_MOTION_STATE_EFFECTOR_H
+
+#include <Eigen/Dense>
+#include "simulation/dynamics/_GeneralModuleFiles/stateEffector.h"
+#include "simulation/dynamics/_GeneralModuleFiles/stateData.h"
+#include "architecture/_GeneralModuleFiles/sys_model.h"
+#include "architecture/utilities/avsEigenMRP.h"
+
+#include "architecture/msgPayloadDefC/ArrayMotorTorqueMsgPayload.h"
+#include "architecture/msgPayloadDefC/SCStatesMsgPayload.h"
+#include "architecture/msgPayloadDefC/PrescribedMotionMsgPayload.h"
+#include "architecture/messaging/messaging.h"
+
+#include "architecture/utilities/bskLogging.h"
+
+/*! @brief prescribed motion state effector class */
+class PrescribedMotionStateEffector: public StateEffector, public SysModel {
+public:
+    // GIVEN QUANTITIES FROM USER PYTHON INTERFACE
+    double mass;
+    Eigen::Matrix3d IHubBc_B;
+    Eigen::Matrix3d IPntFc_F;
+    Eigen::Vector3d r_MB_B;
+    Eigen::Vector3d r_FcF_F;
+    double theta_FBInit;
+    double thetaDot_FBInit;
+    double u_B;
+    Eigen::Matrix3d dcm_F0B;
+    int rotAxisNum;
+
+    Message<PrescribedMotionMsgPayload> prescribedMotionOutMsg;
+    Message<SCStatesMsgPayload> prescribedMotionConfigLogOutMsg;
+    ReadFunctor<ArrayMotorTorqueMsgPayload> motorTorqueInMsg;
+    BSKLogger bskLogger;
+
+private:
+    static uint64_t effectorID;
+
+    // GIVEN QUANTITIES FROM USER PYTHON INTERFACE IN BODY FRAME
+    Eigen::Matrix3d IPntFc_B; // OR DEFINE WITHIN MEMBER FUNCTION     Eigen::Vector3d IPntFc_B = this->dcmBF*this->IPntFc_F
+    Eigen::Vector3d r_FB_B; // DEFINE WITHIN MEMBER FUNCTION     Eigen::Vector3d r_FB_B = r_FM_B + this->r_MB_B
+    Eigen::Vector3d r_FcF_B; // OR DEFINE WITHIN MEMBER FUNCTION     Eigen::Vector3d r_FcF_B = this->dcmBF*this->r_FcF_F
+
+    // DCMs
+    Eigen::Matrix3d dcm_BN;
+    Eigen::Matrix3d dcm_BF;
+    Eigen::Matrix3d dcm_BM;
+
+    // PRESCRIBED PARAMETERS
+    Eigen::Vector3d r_FM_M;
+    Eigen::Vector3d rPrime_FM_M;
+    Eigen::Vector3d rPrimePrime_FM_M;
+    Eigen::Vector3d omega_FB_F;
+    Eigen::Vector3d omegaPrime_FB_F;
+
+    // PRESCRIBED PARAMETERS IN BODY FRAME
+    Eigen::Vector3d r_FM_B;
+    Eigen::Vector3d rPrime_FM_B;
+    Eigen::Vector3d rPrimePrime_FM_B;
+    Eigen::Vector3d omega_FB_B;
+    Eigen::Vector3d omegaPrime_FB_B;
+
+    // Vector quantities
+    Eigen::Vector3d r_FcB_B;
+    Eigen::Vector3d r_FcM_B;
+    Eigen::Vector3d rPrime_FcF_B;
+    Eigen::Vector3d rPrime_FcM_B;
+    Eigen::Vector3d rPrime_FcB_B;
+    Eigen::Vector3d rPrimePrime_FcB_B;
+    Eigen::Vector3d omega_BN_B;
+    Eigen::Vector3d omega_FN_B;
+    Eigen::Vector3d omega_FM_B;
+    Eigen::Vector3d omega_MB_B;
+    Eigen::Vector3d rDot_FcF_B;
+    Eigen::Vector3d rDot_FcB_B;
+    Eigen::MRPd sigma_BN;
+    Eigen::Vector3d sigma_FB;
+
+    // Matrix quantities
+    Eigen::Matrix3d rTilde_FcB_B;
+    Eigen::Matrix3d omegaTilde_BN_B;
+    Eigen::Matrix3d IPrimePntFc_B;
+
+    // Effector body properties
+    Eigen::Vector3d r_FcN_N;
+    Eigen::Vector3d v_FcN_N;
+    Eigen::Vector3d sigma_FN;
+    Eigen::Vector3d omega_FN_F;
+
+    // States
+    StateData *hubSigma;
+    StateData *hubOmega;
+    StateData *hubPosition;
+    StateData *hubVelocity;
+    Eigen::MatrixXd *c_B;
+    Eigen::MatrixXd *cPrime_B;
+
+public:
+    PrescribedMotionStateEffector();    //!< -- Constructor
+    ~PrescribedMotionStateEffector();   //!< -- Destructor
+    void Reset(uint64_t CurrentClock);                   //!< -- Method for reset
+    void writeOutputStateMessages(uint64_t CurrentClock);   //!< -- Method for writing the output messages
+	void UpdateState(uint64_t CurrentSimNanos);             //!< -- Method for updating information
+    void registerStates(DynParamManager& statesIn);         //!< -- Method for registering the effector's states
+    void linkInStates(DynParamManager& states);             //!< -- Method for getting access to other states
+    void updateContributions(double integTime, BackSubMatrices & backSubContr, Eigen::Vector3d sigma_BN, Eigen::Vector3d omega_BN_B);  //!< -- Method for back-substitution contributions
+    void computeDerivatives(double integTime, Eigen::Vector3d rDDot_BN_N, Eigen::Vector3d omegaDot_BN_B, Eigen::Vector3d sigma_BN);                         //!< -- Method for effector to compute its derivatives
+    void updateEffectorMassProps(double integTime);         //!< -- Method for giving the s/c the HRB mass props and prop rates
+    void updateEnergyMomContributions(double integTime, Eigen::Vector3d & rotAngMomPntCContr_B, double & rotEnergyContr, Eigen::Vector3d omega_BN_B);       //!< -- Method for computing energy and momentum for effectors
+    void prependSpacecraftNameToStates();                   //!< Method used for multiple spacecraft
+    void computePrescribedMotionInertialStates();               //!< Method for computing the effector's states
+    void computePrescribedParameters(double integTime);
+
+};
+
+#endif /* PRESCRIBED_MOTION_STATE_EFFECTOR_H */
